155ce23a301170a5fb8329900c8fe8f009b053e0
hristogochev <hristogochev35@gmail.com>
2023-05-24 02:34:12 +0300

    Fixed minification for tailwindcss

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 mkdir -p ./dist
-npx tailwindcss -i ./src/input.css -o ./src/build.css --minify
+npx tailwindcss -i ./src/input.css -o ./dist/build.css --minify
 cp ./CNAME ./dist
 cp ./src/index.html ./dist
-cp ./src/build.css ./dist
 cp ./src/scripts.js ./dist
 cp -r ./src/img ./dist/img
 cp ./src/privacy.html ./dist
