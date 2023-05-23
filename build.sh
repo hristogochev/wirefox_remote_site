8136302c433ad550f09081b84caa5b7f40a80233
hristogochev <hristogochev35@gmail.com>
2023-05-24 02:02:30 +0300

    Reordered inputs

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 mkdir -p ./dist
-npx tailwindcss -i ./input.css -o ./build.css
+npx tailwindcss -i ./src/input.css -o ./src/build.css
 cp ./CNAME ./dist
-cp ./index.html ./dist
-cp ./build.css ./dist
-cp ./scripts.js ./dist
-cp -r ./img ./dist/img
-cp ./privacy.html ./dist
-cp -r ./versions ./dist/versions
+cp ./src/index.html ./dist
+cp ./src/build.css ./dist
+cp ./src/scripts.js ./dist
+cp -r ./src/img ./dist/img
+cp ./src/privacy.html ./dist
+cp -r ./src/versions ./dist/versions
