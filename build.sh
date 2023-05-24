f62e687f915491c798287cc436f3c62ff79aa353
hristogochev <hristogochev35@gmail.com>
2023-05-24 04:39:09 +0300

    Updated index.html, package.json and build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ npx tailwindcss -i ./src/input.css -o ./dist/build.css --minify
 cp ./CNAME ./dist
 cp ./src/index.html ./dist
 cp ./src/scripts.js ./dist
-cp -r ./src/img ./dist/img
+cp -r ./src/img ./dist
 cp ./src/privacy.html ./dist
 cp -r ./src/versions ./dist/versions
